04ae7b339d8bf144f6f68c4c483c80d5bd467427
Colton Neal <colton.neal2020@gmail.com>
2025-12-27 19:56:39 -0600

    Polyfill Buffer and process for Solana browser support

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,9 @@
+import { Buffer } from "buffer";
+import process from "process";
+
+window.Buffer = Buffer;
+window.process = process;
+
 import React from "react";
 import "@solana/wallet-adapter-react-ui/styles.css";
 import ReactDOM from "react-dom/client";
