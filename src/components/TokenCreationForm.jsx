be288e9f1d229f2c87ffbaf2c0df1792bc8262f3
Colton Neal <colton.neal2020@gmail.com>
2025-12-27 23:00:09 -0600

    Fix Metaplex metadata program ID in frontend

--- src/components/TokenCreationForm.jsx
+++ src/components/TokenCreationForm.jsx
@@ -19,10 +19,11 @@ import {
 } from "@solana/spl-token";
 import { FaInfoCircle, FaMoneyBillWave, FaUsers } from "react-icons/fa";
 import { getAuth } from "../authStorage";
-import {
-  createCreateMetadataAccountV3Instruction,
-  PROGRAM_ID as TOKEN_METADATA_PROGRAM_ID,
-} from "@metaplex-foundation/mpl-token-metadata";
+import { createCreateMetadataAccountV3Instruction } from "@metaplex-foundation/mpl-token-metadata";
+  const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
+  "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+);
+
 
 export default function TokenCreationForm() {
   const wallet = useWallet();
